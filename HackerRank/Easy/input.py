2cd4247d4d0770e8cfcb6ebbdc07f5d4a08dbed0
salah mohamed <salahhamada63@gmail.com>
2024-02-01 10:54:55 +0200

    2/1/24

---
+++ HackerRank/Easy/input.py
@@ -0,0 +1,11 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+x , b = map(int , input().split())
+if eval(input()) == b :
+    print(True)
+else : 
+    print(False)
+    
+# in ->  1 4
+# in ->  x**3 + x**2 + x + 1
+# out -> True
